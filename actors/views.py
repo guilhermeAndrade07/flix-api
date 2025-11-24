4d3f9021505f227a6ea7df2ba6099a8895ceab9b
Guilherme Andrade <gui.morais.andrade@gmail.com>
2025-11-24 14:39:36 -0300

    New Movies and Reviews files, full CRUD | URL versioning and resources

--- actors/views.py
+++ actors/views.py
@@ -3,7 +3,6 @@ from actors.models import Actor
 from actors.serializers import ActorSerializer
 
 
-
 class ActorCreateListView(generics.ListCreateAPIView):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     
     'genres',
     'actors',
+    'movies',
+    'reviews',
 ]
 
 MIDDLEWARE = [
