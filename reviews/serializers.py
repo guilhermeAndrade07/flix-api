1ad2c6b642b7823a6b6a7215f725a41e1327bf14
Guilherme Andrade <gui.morais.andrade@gmail.com>
2025-11-25 14:28:32 -0300

    Completed module the Serializers

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -7,3 +7,8 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
+
+    def validate_comment(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('O comentário deve ter no máximo 200 caracteres.')
+        return value
